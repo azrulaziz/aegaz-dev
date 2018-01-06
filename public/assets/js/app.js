c299c29cee5665b5c65cf3e2c0be25851fecdd26
Azrul Aziz <azrultres@gmail.com>
2018-01-06 19:06:24 +0000

    added preloader page

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,3 +10,13 @@ function closeSlideMenu() {
     document.getElementById('side-menu').style.width = '0';
     
 }
+
+
+// preloader script
+
+$('.preload-logo').show("fade", 2500).delay(1000).hide("slide", {direction: 'up'}, 250);
+$('.preloader').delay(3000).slideUp(800, function() {
+    $('html, body').css({
+        'overflow': 'initial'
+    });
+});
